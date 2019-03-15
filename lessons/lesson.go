b7a0728484a9d62a62ac188095f148bfff8b3a50
emreo <emreodabas@gmail.com>
2019-03-16 00:00:20 +0300

    bf home dir selection

--- lessons/lesson.go
+++ lessons/lesson.go
@@ -1,24 +1,68 @@
 package lessons
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func Init(filePath string) {
+	// Open our jsonFile
+	jsonFile, err := os.Open(filePath)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println("ERROR!!")
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened " + filePath)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	fmt.Println(string(byteValue))
+	// we initialize our Users array
+	var lesson lesson
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &lesson)
+	fmt.Println(lesson)
+
+	showDescriptions(lesson.Descriptions)
+	showInteractions(lesson.InteractiveActions)
+	showQuizes(lesson.Quiz)
+}
+func showQuizes(questions []question) {
 
 }
+func showInteractions(actions []interactiveAction) {
 
-func new(card descriptionCard, action interactiveAction, question2 question) lesson {
+}
+func showDescriptions(cards []descriptionCard) {
 
 }
 
 type lesson struct {
-	descriptions       []descriptionCard
-	interactiveActions []interactiveAction
-	quiz               []question
+	Descriptions       []descriptionCard   `json:"descriptions"`
+	InteractiveActions []interactiveAction `json:"interactiveActions"`
+	Quiz               []question          `json:"quiz"`
 }
 
 type descriptionCard struct {
+	MainHeader string `json:"mainHeader"`
+	Header     string `json:"header"`
+	Data       string `json:"data"`
 }
 
 type interactiveAction struct {
 }
 
 type question struct {
+	Question string   `json:"question"`
+	Options  []string `json:"options"`
+	Answer   string   `json:"answer"`
 }
--- main.go
+++ main.go
@@ -2,29 +2,88 @@ package main
 
 import (
 	"fmt"
+	"github.com/emreodabas/kubectl-demo/lessons"
+	"github.com/ktr0731/go-fuzzyfinder"
+	"log"
 	"os"
 	"os/exec"
-	"strconv"
+	"os/user"
 	"strings"
 	"time"
 )
 
-var k3sLink = "https://github.com/rancher/k3s/releases/download/v0.2.0-rc5/k3s"
-var kubeHome = "/home/emreo/.kube/"
+var k3sLink = " https://github.com/rancher/k3s/releases/download/v0.2.0-rc5/k3s"
+var usr, _ = user.Current()
+var kubeHome = usr.HomeDir + "/.kube/"
+var path = kubeHome + "k3s"
 var kubeConfigCmd = "--kubeconfig " + kubeHome + "k3s.yaml"
 
-var demoList = []string{
-	"Basic Commands (Beginner) ",
-	"Basic Commands (Intermediate) ",
-	"Deploy Commands ",
-	"Cluster Management Commands ",
-	"Troubleshooting and Debugging Commands ",
-	"Advanced Commands ",
-	"Settings Commands ",
-	"Other Commands ",
+var selections = []selection{
+	{"intro.json", "1-> Introduction to kubectl", "kubectl controls the Kubernetes cluster manager.   \n" +
+		"  \n" +
+		"Find more information at: https://kubernetes.io/docs/reference/kubectl/overview/  \n" +
+		"  \n" +
+		"Usage:  \n" +
+		"  kubectl [flags] [options]  \n" +
+		"Use \"kubectl <command> --help\" for more information about a given command.  \n" +
+		"Use \"kubectl options\" for a list of global command-line options (applies to all commands). "},
+	{"lessons/basic-commands-beginner.json", "2-> Basic Commands (Beginner)", "\n \t create         Create a resource from a file or from stdin.  \n " +
+		"\t expose         Take a replication controller, service, deployment or pod and  expose it as a new Kubernetes Service  \n " +
+		"\t run            Run a particular image on the cluster  \n " +
+		"\t set            Set specific features on objects  \n " +
+		"   \n "},
+	{"basic-commands-intermediate.json", "3-> Basic Commands (Intermediate) ", "\n \t explain        Documentation of resources  \n " +
+		"\t get            Display one or many resources  \n " +
+		"\t edit           Edit a resource on the server  \n " +
+		"\t delete         Delete resources by filenames, stdin, resources and names, or by resources and label selector  \n " +
+		"   \n "},
+	{"deploy-commands.json", "4-> Deploy Commands ", "\n \t rollout        Manage the rollout of a resource  \n " +
+		"\t scale          Set a new size for a Deployment, ReplicaSet, Replication  Controller, or Job  \n " +
+		"\t autoscale      Auto-scale a Deployment, ReplicaSet, or ReplicationController  \n " +
+		"   \n "},
+	{"cluster-managements-commands.json", "5-> Cluster Management Commands ", "\n \t certificate    Modify certificate resources.  \n " +
+		"\t cluster-info   Display cluster info  \n " +
+		"\t top            Display Resource (CPU/Memory/Storage) usage.  \n " +
+		"\t cordon         Mark node as unschedulable  \n " +
+		"\t uncordon       Mark node as schedulable  \n " +
+		"\t drain          Drain node in preparation for maintenance  \n " +
+		"\t taint          Update the taints on one or more nodes  \n " +
+		"   \n "},
+	{"troubleshooting-debugging-commands.json", "6-> Troubleshooting and Debugging Commands ", "\n \t describe       Show details of a specific resource or group of resources  \n " +
+		"\t logs           Print the logs for a container in a pod  \n " +
+		"\t attach         Attach to a running container  \n " +
+		"\t exec           Execute a command in a container  \n " +
+		"\t port-forward   Forward one or more local ports to a pod  \n " +
+		"\t proxy          Run a proxy to the Kubernetes API server  \n " +
+		"\t cp             Copy files and directories to and from containers.  \n " +
+		"\t auth           Inspect authorization  \n " +
+		"   \n "},
+	{"advanced-commands.json", "7-> Advanced Commands ", "\n \t diff           Diff live version against would-be applied version  \n " +
+		"\t apply          Apply a configuration to a resource by filename or stdin  \n " +
+		"\t patch          Update field(s) of a resource using strategic merge patch  \n " +
+		"\t replace        Replace a resource by filename or stdin  \n " +
+		"\t wait           Experimental: Wait for a specific condition on one or many resources.  \n " +
+		"\t convert        Convert config files between different API versions  \n " +
+		"   \n "},
+	{"settings-commands.json", "8-> Settings Commands ", "\n \t label          Update the labels on a resource  \n " +
+		"\t annotate       Update the annotations on a resource  \n " +
+		"\t completion     Output shell completion code for the specified shell (bash or zsh)  \n " +
+		"   \n "},
+	{"other-commands.json", "9-> Other Commands ", "\n \t api-resources  Print the supported API resources on the server  \n " +
+		"\t api-versions   Print the supported API versions on the server, in the form of \"group/version\"  \n " +
+		"\t config         Modify kubeconfig files  \n " +
+		"\t plugin         Provides utilities for interacting with plugins.  \n " +
+		"\t version        Print the client and server version information  \n "},
+}
+
+type selection struct {
+	filePath    string
+	name        string
+	description string
 }
 
 func main() {
+	fmt.Println(kubeHome)
 	initK3s()
 }
 
@@ -38,7 +97,7 @@ func initK3s() {
 	if startK3sServer() {
 		fmt.Println("Server succesfully started")
 	} else {
-		fmt.Println("Server start error. You could check details in ~/.kube/k3s.logs ")
+		fmt.Println("Server start error. You could check details in ~/.kube/k3s.log ")
 		return
 	}
 
@@ -63,13 +122,11 @@ func startDemo() {
 	} else {
 		item = showDemoList()
 	}
-	showDemo(item)
+	showDemo(selections[item])
 	//commandRun("kubectl get ns " + kubeConfigCmd)
 }
-func showDemo(i int) {
-}
-
-func loadLessonData(i int) {
+func showDemo(s selection) {
+	lessons.Init(s.filePath)
 
 }
 
@@ -81,7 +138,23 @@ func isDemoStarted() bool {
 }
 
 func showDemoList() int {
-	return Choose("", demoList)
+	idx, err := fuzzyfinder.FindMulti(
+		selections,
+		func(i int) string {
+			return selections[i].name
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return fmt.Sprintf("Name: %s \nDescription: %s",
+				strings.SplitAfter(selections[i].name, "> ")[1],
+				selections[i].description)
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return idx[0]
 }
 
 func startK3sServer() bool {
@@ -126,14 +199,14 @@ func installK3s() {
 }
 
 func isInstalled() bool {
-
-	return fileExists(kubeHome + "k3s")
+	fmt.Println(path)
+	return fileExists(path)
 }
 
 // Confirm continues prompting until the input is boolean-ish.
-func Confirm(prompt string, args ...interface{}) bool {
+func Confirm(promptValue string, args ...interface{}) bool {
 	for {
-		switch String(prompt, args...) {
+		switch Prompt(promptValue, args...) {
 		case "Yes", "yes", "y", "Y":
 			return true
 		case "No", "no", "n", "N":
@@ -142,8 +215,7 @@ func Confirm(prompt string, args ...interface{}) bool {
 	}
 }
 
-// String prompt.
-func String(prompt string, args ...interface{}) string {
+func Prompt(prompt string, args ...interface{}) string {
 	var s string
 	fmt.Printf(prompt+": ", args...)
 	fmt.Scanln(&s)
@@ -171,44 +243,43 @@ func fileExists(name string) bool {
 	return true
 }
 
-func Choose(prompt string, list []string) int {
-	fmt.Println()
-	for i, val := range list {
-		fmt.Printf("  %d) %s\n", i+1, val)
-	}
+//func Choose(promptValue string, list map[string]string) string {
+//	fmt.Println()
+//	for i, val := range list {
+//		fmt.Printf("  %d) %s\n", i+1, val)
+//	}
+//
+//	fmt.Println()
+//	i := ""
+//
+//	for {
+//		s := Prompt(promptValue)
+//
+//		n, err := strconv.Atoi(s)
+//		if err == nil {
+//			if n > 0 && n <= len(list) {
+//				i = n - 1
+//				break
+//			} else {
+//				continue
+//			}
+//		}
+//
+//		// value
+//		i = indexOf(s, list)
+//		if i != "" {
+//			break
+//		}
+//	}
+//
+//	return i
+//}
 
-	fmt.Println()
-	i := -1
-
-	for {
-		s := String(prompt)
-
-		// index
-		n, err := strconv.Atoi(s)
-		if err == nil {
-			if n > 0 && n <= len(list) {
-				i = n - 1
-				break
-			} else {
-				continue
-			}
-		}
-
-		// value
-		i = indexOf(s, list)
-		if i != -1 {
-			break
-		}
-	}
-
-	return i
-}
-
-func indexOf(s string, list []string) int {
+func indexOf(s string, list map[string]string) string {
 	for i, val := range list {
 		if val == s {
 			return i
 		}
 	}
-	return -1
+	return ""
 }
